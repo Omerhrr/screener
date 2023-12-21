b1ff0ab5861318fc5c39ac1fe9d9badf65247ab0
BZ <114144177+Omerhrr@users.noreply.github.com>
2023-12-21 13:27:35 +0100

    Update screener.py

--- screener.py
+++ screener.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import numpy as np
 from alpha_vantage.fundamentaldata import FundamentalData
 from stocknews import StockNews 
+import africastalking
 # import pygwalker as pyg
 background_color = '''
 <style>
@@ -14,12 +15,77 @@ background_color = '''
 '''
 st.markdown(background_color, unsafe_allow_html=True)
 
+africastalking.initialize(username="SANDBOX", api_key="8e2c0773b93db3a6c8aeebe968efcce313e7e72d9ac850b97a7fb45af77f130d")
+
 
 st.title("Stock Dashboard")
 ticker = st.sidebar.text_input("Ticker")
 start_date = st.sidebar.date_input("Start Date")
 end_date = st.sidebar.date_input("End Date")
 
+# Function to send SMS alerts
+def send_sms_alert(recipient, message):
+    try:
+        # Send SMS
+        sms = africastalking.SMS
+        sms_payload = {
+            "to": [f"+{recipient}"],
+            "message": message,
+            "from_": "AFRICASTALKING",  # Replace with your sender ID
+        }
+        response = sms.send(sms_payload)
+        st.success("SMS sent successfully!")
+        st.write(response)
+
+    except Exception as e:
+        st.error(f"Error sending SMS: {e}")
+
+# Function to send Bulk SMS alerts
+def send_bulk_sms(recipients, message):
+    try:
+        # Send Bulk SMS
+        sms = africastalking.SMS
+        sms_payload = {
+            "to": recipients,
+            "message": message,
+            "from_": "AFRICASTALKING",  # Replace with your sender ID
+        }
+        response = sms.send(sms_payload)
+        st.success("Bulk SMS sent successfully!")
+        st.write(response)
+
+    except Exception as e:
+        st.error(f"Error sending Bulk SMS: {e}")
+
+# Function to send news alerts
+def send_news_alerts(news_df):
+    try:
+        # Send News Alerts
+        for i in range(10):
+            news_message = f"{news_df['published'][i]}\n{news_df['title'][i]}\n{news_df['summary'][i]}"
+            send_sms_alert("RECIPIENT_PHONE_NUMBER", news_message)  # Replace with recipient's phone number
+
+        st.success("News alerts sent successfully!")
+
+    except Exception as e:
+        st.error(f"Error sending News Alerts: {e}")
+
+# Function to make voice call
+def make_voice_call(recipient):
+    try:
+        # Make Voice Call
+        voice = africastalking.Voice
+        voice_payload = {
+            "from_": "AFRICASTALKING",  # Replace with your sender ID
+            "to": [f"+{recipient}"],
+            "url": "URL_TO_VOICE_FILE_OR_API_ENDPOINT",  # Replace with your voice file or API endpoint
+        }
+        response = voice.call(voice_payload)
+        st.success("Voice Call initiated successfully!")
+        st.write(response)
+
+    except Exception as e:
+        st.error(f"Error making Voice Call: {e}")
 
 try:
     data= yf.download(ticker, start=start_date, end=end_date)
